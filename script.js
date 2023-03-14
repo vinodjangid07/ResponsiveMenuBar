5c42b9890f73caf362738ee7d770e1bd755e58a6
Vinod Jangid <86096184+vinodjangid07@users.noreply.github.com>
2023-03-14 21:56:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function hamburgerMenu() {
+    document.getElementById("tabs_button").classList.toggle("show");
+    document.body.classList.toggle("stopscrolling");
+    document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
+    document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
+    document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
+
+    
+  }
+
+
+
